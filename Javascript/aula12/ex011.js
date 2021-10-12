a8ddc882b606f47f0d90fcf8fa5898c90fd062f5
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2021-10-11 23:49:57 -0300

    aulas do dia 11/10 upload

---
+++ Javascript/aula12/ex011.js
@@ -0,0 +1,14 @@
+
+//condições simples
+var idade = 15
+if (idade < 16){
+console.log('não vota')
+}else if (idade<18 || idade > 65){
+    console.log('voto opcional')
+    } else{
+        console.log('voto obrigatorio')
+    }
+
+
+
+
