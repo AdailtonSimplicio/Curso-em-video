a19a70ebdf38ae9906de8f6d3fb16b11d001db64
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2021-10-17 00:53:32 -0300

    atualizando 17/10/2021

--- Javascript/aula14ex/ex017/script.js
+++ Javascript/aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert ('Olá')
+
+function calcular() {
+    let num = document.getElementById('txtd')
+    let tb = document.getElementById('seltab')
+
+    if (num.value.length == 0){ // se não houver nada digitado cham o alerta
+            window.alert (`Por favor, digite um número!`)
+        
+    }else {
+        let n = Number (num.value)// convertendo string para numero
+        let c = 1
+        tb.innerHTML =""// limpar o campo antes de gerar novo calculo
+            while ( c <= 10 ){
+                let item = document.createElement('option') // criar opção na aba select
+                item.text = ` ${n} x ${c} = ${n*c}`//inserir texto na option da aba select
+                item.value = `tab${c}` // para saber qual tabela foi selecionada
+                tb.appendChild(item)
+                c++
+        }
+    }
+}
+
